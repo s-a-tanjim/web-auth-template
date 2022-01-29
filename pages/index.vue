fbd4c1d00146ad2e36a3c2f931eb9d0a6b12a5e8
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2022-01-29 22:45:08 +0600

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title" style="text-align: center">Nuxt JWT Auth Home</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
